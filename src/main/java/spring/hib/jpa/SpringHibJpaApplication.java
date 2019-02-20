fe831070c1e92d0d2fbeecea760357944c7b8775
pavel <pavel@pavel>
2019-02-20 22:01:38 +0100

    tutor

--- src/main/java/spring/hib/jpa/SpringHibJpaApplication.java
+++ src/main/java/spring/hib/jpa/SpringHibJpaApplication.java
@@ -3,6 +3,16 @@ package spring.hib.jpa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * tutorial
+ * https://youtu.be/MaI0_XdpdP8
+ * 
+ * h2 database
+ * http://localhost:8080/h2-console/
+ * 
+ * @author pavel
+ *
+ */
 @SpringBootApplication
 public class SpringHibJpaApplication {
 
